7b1ad47a493d3b66992988eb00633ff4d0ee5515
CHARLOTTE <creitan@uvm.edu>
2024-10-23 10:31:52 -0400

    First Commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function vampire(name, age, bloodType, height) {
+    this.name = name;
+    this.age = age;
+    this.bloodType = bloodType;
+    this.height = height;
+}
+
+function zombie(name, age, bloodType, height) {
+    this.name = name;
+    this.age = age;
+    this.bloodType = bloodType;
+    this.height = height;
+}
+
+function gorgon(name, age, bloodType, height) {
+    this.name = name;
+    this.age = age;
+    this.bloodType = bloodType;
+    this.height = height;
+}
+
+const vampireElement = document.getElementById('vampire');
+const zombieElement = document.getElementById('zombie');
+const gorgonElement = document.getElementById('gorgon');
